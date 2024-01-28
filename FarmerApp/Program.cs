d18a9badc18216f6e505057f67c5ede369bea1f1
rafik.tserunyan <rafiktserunyan@gmail.com>
2024-01-28 15:19:52 +0400

    Comit

--- FarmerApp/Program.cs
+++ FarmerApp/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FarmerAPI", Version = "v1.1.4" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FarmerAPI", Version = "v1.1.6" });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Description = "JWT Authorization",
