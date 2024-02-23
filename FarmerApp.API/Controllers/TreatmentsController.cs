f0b9a7392219baae491a87de1a262298e669fb57
rafik.tserunyan <rafiktserunyan@gmail.com>
2024-02-23 11:56:41 +0400

    FIx

--- FarmerApp.API/Controllers/TreatmentsController.cs
+++ FarmerApp.API/Controllers/TreatmentsController.cs
@@ -9,6 +9,7 @@ using FarmerApp.Data.Entities;
 using FarmerApp.Data.Specifications.Treatment;
 using FarmerApp.Models.ViewModels.RequestModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace FarmerApp.Controllers
 {
@@ -24,6 +25,17 @@ namespace FarmerApp.Controllers
             var treatments = await _service.GetAll(new TreatmentsWithDepsSpecification(), query);
 
             return _mapper.Map<PagedResult<TreatmentResponseModel>>(treatments);
+        }
+
+        public override async Task<ActionResult<TreatmentResponseModel>> Create([BindRequired, FromBody] TreatmentRequestModel model)
+        {
+            var businessModel = _mapper.Map<TreatmentModel>(model);
+            businessModel.UserId = UserId;
+
+            var data = await _service.Add(businessModel);
+
+            // Fix this when depth builder is available
+            return await ReadById((int)data.Id);
         }
     }
 }
