7d559e5cf17ed8fcef2684f3247ff833e362c327
rafik.tserunyan <rafiktserunyan@gmail.com>
2024-02-23 11:02:05 +0400

    Added InvestmentWithDepsSpecification

---
+++ FarmerApp.Data/Specifications/Investments/InvestmentWithDepsSpecification.cs
@@ -0,0 +1,13 @@
+﻿using FarmerApp.Data.Entities;
+using FarmerApp.Data.Specifications.Common;
+
+namespace FarmerApp.Data.Specifications.Investments
+{
+    public class InvestmentWithDepsSpecification : BaseSpecification<InvestmentEntity>
+    {
+        public InvestmentWithDepsSpecification()
+        {
+            AddInclude(x => x.Investor);
+        }
+    }
+}
