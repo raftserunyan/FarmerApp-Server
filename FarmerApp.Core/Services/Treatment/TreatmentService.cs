8020967bd9df0335a1ad78ca5cb8ae3898e4fcec
Rafik Tserunyan <rafiktserunyan@royteam.tech>
2025-08-11 10:54:16 +0400

    Added filtering by treated product ids in treatments

--- FarmerApp.Core/Services/Treatment/TreatmentService.cs
+++ FarmerApp.Core/Services/Treatment/TreatmentService.cs
@@ -1,7 +1,11 @@
 using AutoMapper;
 using FarmerApp.Core.Models.Treatment;
+using FarmerApp.Core.Query;
+using FarmerApp.Core.Query.DynamicSortingBuilder;
 using FarmerApp.Core.Services.Common;
+using FarmerApp.Core.Wrappers;
 using FarmerApp.Data.Entities;
+using FarmerApp.Data.Specifications.Common;
 using FarmerApp.Data.Specifications.Product;
 using FarmerApp.Data.Specifications.Treatment;
 using FarmerApp.Data.UnitOfWork;
@@ -14,6 +18,57 @@ namespace FarmerApp.Core.Services.Treatment
         {
         }
 
+        public async override Task<PagedResult<TreatmentModel>> GetAll(ISpecification<TreatmentEntity> specification = null, BaseQueryModel query = null, bool includeDeleted = false, int depth = 1, IEnumerable<string> propertyTypesToExclude = null)
+        {
+            var treatedProductIdsFilter = query.AndFilters?.Where(x => x.Property == "treatedProductIds").FirstOrDefault();
+            if (treatedProductIdsFilter != null)
+            {
+                query.AndFilters.Remove(treatedProductIdsFilter);
+            }
+
+            specification ??= new EmptySpecification<TreatmentEntity>();
+            int? total = null;
+
+            if (query is not null)
+            {
+                FilterResults(specification, query);
+
+                total = await _uow.Repository<TreatmentEntity>().Count(specification, includeDeleted);
+
+                // Exclude .User if it isn't already excluded
+                propertyTypesToExclude = (propertyTypesToExclude ?? Enumerable.Empty<string>())
+                    .Concat(new[] { nameof(UserEntity) })
+                    .Distinct();
+
+                IncludeDependenciesByDepth(specification, depth, propertyTypesToExclude);
+            }
+
+            var treatments = await _uow.Repository<TreatmentEntity>().GetAllBySpecification(specification, includeDeleted);
+
+            if (treatedProductIdsFilter is not null)
+            {
+                treatments = treatments
+                    .Where(t => t.Products.Any(p => treatedProductIdsFilter.Value.Contains(p.Id.ToString())))
+                    .ToList();
+
+                total = treatments.Count;
+            }
+
+            treatments = ApplyOrdering(treatments, query.Orderings);
+
+            treatments = treatments
+                .Skip((query.PageNumber - 1) * query.PageSize)
+                .Take(query.PageSize).ToList();
+
+            return new PagedResult<TreatmentModel>
+            {
+                Results = _mapper.Map<List<TreatmentModel>>(treatments),
+                Total = total ?? treatments.Count,
+                PageNumber = query?.PageNumber ?? 1,
+                PageSize = query?.PageSize ?? (total ?? treatments.Count)
+            };
+        }
+        
         public override async Task<TreatmentModel> Add(TreatmentModel model, int depth = 1, IEnumerable<string> propertyTypesToExclude = null)
         {
             var entity = ValidateAndMap(model, "Model to be added was null");
@@ -45,6 +100,30 @@ namespace FarmerApp.Core.Services.Treatment
             await _uow.SaveChangesAsync();
 
             return await GetById(entity.Id, false, depth, propertyTypesToExclude);
+        }
+
+
+        private static List<TreatmentEntity> ApplyOrdering(IEnumerable<TreatmentEntity> list, List<OrderingItem> orderings)
+        {
+            if (list == null || orderings == null || !list.Any() || !orderings.Any())
+                return list.ToList();
+
+            var orderingExpressionsFromQuery = DynamicSortingBuilder.BuildOrderingFunc<TreatmentEntity>(orderings);
+
+            var isAscending = orderings.First().IsAscending;
+            var isFirst = true;
+
+            foreach (var item in orderingExpressionsFromQuery.Select(x => x.Compile()))
+            {
+                if (isAscending)
+                    list = isFirst ? list.OrderBy(item) : ((IOrderedEnumerable<TreatmentEntity>)list).ThenBy(item);
+                else
+                    list = isFirst ? list.OrderByDescending(item) : ((IOrderedEnumerable<TreatmentEntity>)list).ThenByDescending(item);
+
+                isFirst = false;
+            }
+
+            return list.ToList();
         }
     }
 }
